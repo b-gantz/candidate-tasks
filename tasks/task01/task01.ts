e877805bc98dcfaf4bcd26f85cc739eb50917461
Blazej Gantz <blazej.gantz@gmail.com>
2023-06-28 12:58:57 +0200

    Solution of recruitment task

--- tasks/task01/task01.ts
+++ tasks/task01/task01.ts
@@ -1,3 +1,5 @@
+import {describe, expect, test} from '@jest/globals';
+
 class LoginPage {
     login(username: string, password: string, authService: AuthenticationService): boolean {
       let result = false;
@@ -11,3 +13,26 @@ class LoginPage {
       return result;
     }
   }
+
+//below content of login.test.ts
+
+const falseAuthService = {
+    authenticate: (username, password) => false 
+};
+
+const trueAuthService = {
+    authenticate: (username, password) => true
+};
+
+describe('Login function', () => {
+    test('Wrong credentials for login'), () => 
+        expect(login('wrongLogin','wrongPassword', falseAuthService).toBeFalsy;
+    });
+    test('Correct credentials for login'), () => 
+        expect(login('correctLogin','correctPassword', trueAuthService).toBeFalsy;
+    });
+    test('Admin credentials for login with mocked auth responce'), () => 
+        expect(login('admin','admin', authService.mockReturnValueOnce('true')).toBeFalsy;
+    });
+});
+
